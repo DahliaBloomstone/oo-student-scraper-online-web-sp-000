9380f251e84499d300b5ee5bfbcbce90bcedfede
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-08 01:55:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -50,9 +50,9 @@ doc = Nokogiri::HTML(html)
 ##Returns a hash that describes individual students: Twitter URL, Linkedin URL, Github URL, blob URL, profile quote, bio
 student_profiles = {}
 
-    social_link = doc.css(".vitals-container .social-icon-container a")
+social_link = doc.css(".vitals-container .social-icon-container a")
 
-    social_link.each do |element|
+social_link.each do |element|
       if element.attr("href").include?("twitter")
         student_profiles[:twitter] = element.attr('href')
       elsif element.attr("href").include?("linkedin")
